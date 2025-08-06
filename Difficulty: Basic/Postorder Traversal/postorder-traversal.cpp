d380b617822786738ac6934dcca552db3acca35b
Himanshu Yadav <himu09854@gmail.com>
2025-08-06 18:38:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Postorder Traversal/postorder-traversal.cpp
@@ -0,0 +1,25 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}; */
+class Solution {
+  public:
+    // Function to return a list containing the postorder traversal of the tree.
+    vector<int> postOrder(Node* root) {
+        // Your code here
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+    
+    void helper(Node* root , vector<int>& ans){
+        if(root == nullptr)return;
+        helper(root->left , ans);
+        helper(root->right,ans);
+        ans.push_back(root->data);
+    }
+};
