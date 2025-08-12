05d69fd51ba6650435713d4614248fa60c7558c4
Himanshu Yadav <himu09854@gmail.com>
2025-08-13 02:30:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Fibonacci Using Recursion/fibonacci-using-recursion.cpp
+++ Difficulty: Basic/Fibonacci Using Recursion/fibonacci-using-recursion.cpp
@@ -2,10 +2,8 @@ class Solution {
   public:
     int fibonacci(int n) {
         // Code here
-        if(n == 0)return 0;
-        if(n == 1)return 1;
+        if(n <= 1)return n;
         
         return fibonacci(n-1) + fibonacci(n-2);
-        
     }
 };
