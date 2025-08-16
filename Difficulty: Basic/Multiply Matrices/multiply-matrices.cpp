c320d9d09185970f838a7ce9db2346a52e238741
Himanshu Yadav <himu09854@gmail.com>
2025-08-16 19:03:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiply Matrices/multiply-matrices.cpp
@@ -0,0 +1,19 @@
+/*Complete the function below*/
+void multiply(int A[][100], int B[][100], int C[][100], int N) {
+    // add code here.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] = 0;
+            }
+        }
+
+        // Multiply A and B
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+    
+}
