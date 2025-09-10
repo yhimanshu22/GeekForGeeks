38bc63b60e437d1043dce157f0d7110fe601ea7a
Himanshu Yadav <himu09854@gmail.com>
2025-09-10 09:51:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+         int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n;  // default = n (if no element > target)
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1; // search left side
+            } else {
+                low = mid + 1; // search right side
+            }
+        }
+
+        return ans;
+    }
+};
