0e8704377b800e5df176902b3da7d332d3e75816
Himanshu Yadav <himu09854@gmail.com>
2025-09-29 12:29:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Largest in a Stream/kth-largest-in-a-stream.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        vector<int> ans;
+        priority_queue<int, vector<int>, greater<int>> pq; // min-heap
+
+        for(int i = 0; i < n; i++) {
+            pq.push(arr[i]);
+
+            if(pq.size() > k) pq.pop();
+
+            if(pq.size() < k)
+                ans.push_back(-1);
+            else
+                ans.push_back(pq.top());
+        }
+
+        return ans;
+    }
+};
