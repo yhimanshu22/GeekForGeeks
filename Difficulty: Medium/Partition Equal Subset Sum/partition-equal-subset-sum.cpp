be4f4bb23e1edad3bd85b7ff48b92cbb0e7ab9b7
Himanshu Yadav <himu09854@gmail.com>
2025-10-05 02:04:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -5,15 +5,14 @@ public:
         int total = 0;
         for (int x : arr) total += x;
 
-        // If total sum is odd, can't partition equally
+        // odd sum cannot be partitioned equally
         if (total % 2 != 0) return false;
 
         int sumHalf = total / 2;
 
-        // Subset sum DP
         vector<vector<bool>> dp(n+1, vector<bool>(sumHalf+1, false));
 
-        // Base case: sum=0 is always possible
+        // sum=0 can always be formed
         for (int i = 0; i <= n; i++) dp[i][0] = true;
 
         for (int i = 1; i <= n; i++) {
