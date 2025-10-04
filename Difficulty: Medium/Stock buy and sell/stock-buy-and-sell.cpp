59ab75855f4f050b87c0cbb5e6ffce3b307e8918
Himanshu Yadav <himu09854@gmail.com>
2025-10-05 02:06:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int stockBuySell(vector<int> &arr) {
+        int n = arr.size();
+        int profit = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i-1]) {
+                profit += arr[i] - arr[i-1];
+            }
+        }
+
+        return profit;
+    }
+};
