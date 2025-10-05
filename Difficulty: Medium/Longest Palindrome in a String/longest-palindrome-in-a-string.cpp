1b13e7a16cb1edf39d1b319e328799dd8523abab
Himanshu Yadav <himu09854@gmail.com>
2025-10-05 13:28:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     string longestPalindrome(string &s) {
         int n = s.size();
-        if(n == 0) return "";
+        string b = s;
+        reverse(b.begin(), b.end());
 
-        int start = 0, maxLen = 1;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        int maxLen = 0;
+        int endIdx = 0;
 
-        auto expand = [&](int left, int right) {
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                left--; right++;
-            }
-            return right - left - 1; // length of palindrome
-        };
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == b[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
 
-        for(int i = 0; i < n; i++) {
-            int len1 = expand(i, i);     // odd length
-            int len2 = expand(i, i+1);   // even length
-            int len = max(len1, len2);
-
-            if(len > maxLen) {
-                maxLen = len;
-                start = i - (len - 1) / 2;
+                    int revIdx = n - j;
+                    if (revIdx + dp[i][j] - 1 == i - 1) {  // check alignment
+                        if (dp[i][j] > maxLen) {
+                            maxLen = dp[i][j];
+                            endIdx = i - 1;
+                        }
+                    }
+                } else {
+                    dp[i][j] = 0; // substring pattern
+                }
             }
         }
 
-        return s.substr(start, maxLen);
+        return s.substr(endIdx - maxLen + 1, maxLen);
     }
 };
