7b728f37517ba5c0f0ca7ee57e25f16cd8c7f0fd
Himanshu Yadav <himu09854@gmail.com>
2025-10-11 03:31:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -1,43 +1,42 @@
 class Solution {
   public:
-    void dfs(int x, int y, vector<vector<char>>& grid, vector<vector<int>>& visited, int dx[], int dy[]) {
+  
+    void dfs(int x,int y,vector<vector<char>>& grid,vector<vector<int>>& vis){
         int n = grid.size();
-        int m = grid[0].size();
+        int m =grid[0].size();
         
-        visited[x][y] = 1;
+        vis[x][y] = 1;
         
-        for (int k = 0; k < 8; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
+          int dx[8] = {-1,-1,-1,0,0,1,1,1};
+        int dy[8] = {-1,0,1,-1,1,-1,0,1};
+        
+        for(int i=0;i<8;i++){
+            int nx = x+dx[i];
+            int ny = y+ dy[i];
             
-            if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
-                !visited[nx][ny] && grid[nx][ny] == 'L') {
-                dfs(nx, ny, grid, visited, dx, dy);
+            if(nx >= 0 && ny < m && nx < n && ny >=0 && grid[nx][ny] =='L'
+            && !vis[nx][ny]){
+                dfs(nx,ny,grid,vis);
             }
         }
     }
     
     int countIslands(vector<vector<char>>& grid) {
+        // Code here
         int n = grid.size();
-        if (n == 0) return 0;
         int m = grid[0].size();
-
-        vector<vector<int>> visited(n, vector<int>(m, 0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int cnt = 0;
         
-        // 8 directions
-        int dx[8] = {-1, -1, -1,  0, 0, 1, 1, 1};
-        int dy[8] = {-1,  0,  1, -1, 1,-1, 0, 1};
-
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && grid[i][j] == 'L') {
-                    dfs(i, j, grid, visited, dx, dy);
-                    count++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 'L' && !vis[i][j]){
+                    cnt++;
+                    dfs(i,j,grid,vis);
                 }
             }
         }
-        return count;
+        
+        return cnt;
     }
 };
