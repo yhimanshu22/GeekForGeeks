cfcdf7fdc5138de36e735e7fa0f803ee097690a3
Himanshu Yadav <himu09854@gmail.com>
2025-10-11 02:25:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -2,27 +2,42 @@
 
 class Solution {
   public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-        // code here
-        vector<int> visited(V,0);
-        int cnt=0;
-        
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfs(i,adj,visited,V);
-                cnt++;
-            }
-        }
-        return cnt;
-        
+    
+    void dfs(int node,vector<int> adjLs[],vector<int>& vis){
+         vis[node] = 1;
+         
+         for(auto it: adjLs[node]){
+             if(!vis[it]){
+                 dfs(it,adjLs,vis);
+             }
+         }
     }
     
-    void dfs(int node,vector<vector<int>>& adj,vector<int>& visited,int V){
-        visited[node] = 1;
-        for(int j=0;j<V;j++){
-            if(!visited[j] && adj[node][j] == 1){
-                dfs(j,adj,visited,V);
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        vector<int> adjLs[V];
+        
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(i != j && adj[i][j] == 1 ){
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+            
+        }
+        
+        int cnt = 0;
+        vector<int> vis(V,0);
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i,adjLs,vis);
             }
         }
+        
+        return cnt;
+        
     }
 };
