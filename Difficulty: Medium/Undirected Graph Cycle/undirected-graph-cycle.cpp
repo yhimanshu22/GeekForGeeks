d6dc475a082ea2b1e1b77b6725beb6584d7fa288
Himanshu Yadav <himu09854@gmail.com>
2025-10-11 05:14:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,40 +1,38 @@
 class Solution {
-  public:
-    bool isCycle(int V, vector<vector<int>>& edges) {
-        // Code here
-        vector<vector<int>> adj(V);
-        
-        for(auto &e:edges){
-            int u = e[0],v = e[1];
-            
-            adj[u].push_back(v);
-            adj[v].push_back(u); //undirected graph
-        }
-        
-        vector<bool> visited(V,false);
-        
-        //bfs traversal
-        for(int start=0;start < V;start++){
-            if(!visited[start]){
-                queue<pair<int,int>> q; // {node, parent}
-                q.push({start,-1});
-                 visited[start] = true; // mark visited here
-                while(!q.empty()){
-                    auto[node,parent] = q.front();
-                    q.pop();
-                    
-                    for(int neigh:adj[node]){
-                        if(!visited[neigh]){
-                            visited[neigh] = true;
-                            q.push({neigh, node});
-                        }else if(neigh != parent){
-                            return true;// already visited & not parent → cycle
-                        }
-                    }
-                }
-                
+public:
+    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& vis){
+        vis[node] = 1;
+
+        for(auto &neighbor : adj[node]){
+            if(!vis[neighbor]){
+                if(dfs(neighbor, node, adj, vis))
+                    return true;
+            } else if(neighbor != parent){
+                return true;
             }
         }
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V); 
+
+        for(auto &e : edges){
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> vis(V, 0);
+
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(dfs(i, -1, adj, vis))
+                    return true;
+            }
+        }
+
         return false;
     }
 };
