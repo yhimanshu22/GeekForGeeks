88b5c28e5b711e381003ef317a19936367c4b2e8
Himanshu Yadav <himu09854@gmail.com>
2025-10-17 20:05:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -1,32 +1,37 @@
 class Solution {
-public:
+  public:
     string longestPalindrome(string &s) {
-        int n = s.size();
-        string b = s;
-        reverse(b.begin(), b.end());
-
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        int maxLen = 0;
-        int endIdx = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s[i - 1] == b[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-
-                    int revIdx = n - j;
-                    if (revIdx + dp[i][j] - 1 == i - 1) {  // check alignment
-                        if (dp[i][j] > maxLen) {
-                            maxLen = dp[i][j];
-                            endIdx = i - 1;
-                        }
+       
+        int n =s.size();
+        string b  = s;
+        reverse(b.begin(),b.end());
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        int maxLength = 0;
+        int endInd = 0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == b[j-1]){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                    
+                    int startInOriginal = n - j;
+                    
+                    if(startInOriginal + dp[i][j] - 1 == i-1){
+                       if(dp[i][j] > maxLength){
+                        maxLength = dp[i][j];
+                        endInd = i-1;
+                       }
                     }
-                } else {
-                    dp[i][j] = 0; // substring pattern
+                    
+                }else{
+                    dp[i][j] = 0;
                 }
             }
         }
+        
+         return s.substr(endInd - maxLength + 1, maxLength);
 
-        return s.substr(endIdx - maxLen + 1, maxLen);
+        
     }
 };
