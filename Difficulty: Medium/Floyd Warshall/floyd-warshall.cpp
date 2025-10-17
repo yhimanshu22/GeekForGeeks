187156d9270da0f4f9f9f091af1a4ce2d0662660
Himanshu Yadav <himu09854@gmail.com>
2025-10-18 00:26:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -1,13 +1,16 @@
-class Solution {
-public:
-    void floydWarshall(vector<vector<int>> &dist) {
-        int V = dist.size();
+// User function template for C++
 
-        // Step 1: Floyd-Warshall main loop
-        for (int k = 0; k < V; k++) {           // intermediate vertex
-            for (int i = 0; i < V; i++) {       // source vertex
-                for (int j = 0; j < V; j++) {   // destination vertex
-                    if (dist[i][k] != 1e8 && dist[k][j] != 1e8) {
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+      
+        int V = dist.size();
+        const int INF = 100000000;
+        
+        for(int k=0;k< V ; k++){
+            for(int i=0;i<V ; i++){
+                for(int j =0;j< V ; j++){
+                    if(dist[i][k] != INF && dist[k][j] != INF){
                         dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
