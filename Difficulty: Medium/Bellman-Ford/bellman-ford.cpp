fa2ee7caad6202a290f8ce001502cc555d50dd57
Himanshu Yadav <himu09854@gmail.com>
2025-10-18 00:09:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -1,27 +1,42 @@
-class Solution {
-public:
-    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
-        // Step 1: Initialize distances
-        vector<int> dist(V, 1e8);
-        dist[src] = 0;
+// User function Template for C++
 
-        // Step 2: Relax all edges V-1 times
-        for(int i = 0; i < V - 1; i++) {
-            for(auto &e : edges) {
-                int u = e[0], v = e[1], w = e[2];
-                if(dist[u] != 1e8 && dist[u] + w < dist[v])
-                    dist[v] = dist[u] + w;
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> dist(V,1e8);
+        
+        //iterate over V-1 times
+        dist[src] =0;
+        for(int i=0;i<V-1;i++){
+            
+            bool updated = false;
+            
+            for(auto& e:edges){
+                int u = e[0];
+                int v = e[1];
+                int w= e[2];
+                
+                if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                    dist[v] = dist[u]+w;
+                    updated = true;
+                }
             }
+            
+            if(!updated)break;
         }
         
-
-        // Step 3: Check for negative weight cycles
-        for(auto &e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            if(dist[u] != 1e8 && dist[u] + w < dist[v])
-                return {-1}; // Negative cycle detected
+        //iterate for Vth time 
+        for(auto& e:edges){
+            int u = e[0];
+            int v =e[1];
+            int w=e[2];
+            
+            if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                return {-1};
+            }
+            
         }
-
         return dist;
     }
 };
