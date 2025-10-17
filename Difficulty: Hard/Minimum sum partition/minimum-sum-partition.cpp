9f7f47cbdebc4bcac4265de3b0e6e705d1ed1aa9
Himanshu Yadav <himu09854@gmail.com>
2025-10-18 01:30:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -1,29 +1,37 @@
 class Solution {
-public:
+
+  public:
     int minDifference(vector<int>& arr) {
+        // Your code goes here
         int n = arr.size();
         int total = 0;
-        for(int x : arr) total += x;
-
-        vector<vector<bool>> dp(n+1, vector<bool>(total+1, false));
-
-        for(int i = 0; i <= n; i++) dp[i][0] = true;
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= total; j++){
-                if(arr[i-1] <= j)
+        for(int x:arr)total+=x;
+        
+        int target = total/2;
+        
+        vector<vector<int>> dp(n+1,vector<int>(target+1,false));
+        
+        for(int i=0;i<=n;i++)dp[i][0]=true;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(arr[i-1] <= j){
                     dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
-                else
+                    
+                }else{
                     dp[i][j] = dp[i-1][j];
+                }
             }
         }
-
-        int diff = total;
-        for(int j = 0; j <= total/2; j++){
-            if(dp[n][j])
-                diff = min(diff, total - 2*j);
+        
+        int s1=0;
+        for(int i=target;i>=0;i--){
+            if(dp[n][i]){
+                s1=i;
+                break;
+            }
         }
-
-        return diff;
+        
+        return total-2*s1;
     }
 };
