7a0168852ea1db256a3a9c04b928375b627315e6
Himanshu Yadav <himu09854@gmail.com>
2025-10-17 23:06:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
-public:
+  public:
     bool equalPartition(vector<int>& arr) {
+        // code here
         int n = arr.size();
-        int total = 0;
-        for (int x : arr) total += x;
-
-        // odd sum cannot be partitioned equally
-        if (total % 2 != 0) return false;
-
-        int sumHalf = total / 2;
-
-        vector<vector<bool>> dp(n+1, vector<bool>(sumHalf+1, false));
-
-        // sum=0 can always be formed
-        for (int i = 0; i <= n; i++) dp[i][0] = true;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sumHalf; j++) {
-                if (arr[i-1] <= j)
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j - arr[i-1]];
-                else
+        int sum = 0;
+        for(int x:arr)sum+=x;
+        
+        if(sum % 2 == 1)return false;
+        
+        sum = sum/2;
+        
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,false));
+        
+        for(int i=0;i<=n;i++)dp[i][0] = true;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]]; 
+                }else{
                     dp[i][j] = dp[i-1][j];
+                }
             }
         }
-
-        return dp[n][sumHalf];
+        return dp[n][sum];
     }
 };
