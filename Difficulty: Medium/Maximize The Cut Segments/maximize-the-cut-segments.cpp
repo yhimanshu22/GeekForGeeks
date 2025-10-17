c67897f768a329b3b4c731b66f1b740d7abcfb40
Himanshu Yadav <himu09854@gmail.com>
2025-10-17 23:39:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize The Cut Segments/maximize-the-cut-segments.cpp
+++ Difficulty: Medium/Maximize The Cut Segments/maximize-the-cut-segments.cpp
@@ -1,17 +1,19 @@
 class Solution {
-public:
+  public:
+    // Function to find the maximum number of cuts.
     int maximizeTheCuts(int n, int x, int y, int z) {
-        vector<int> dp(n+1, -1);
-        dp[0] = 0; // length 0 → 0 segments
-
-        for(int i = 1; i <= n; i++) {
-            int a = (i >= x && dp[i-x] != -1) ? dp[i-x] + 1 : -1;
-            int b = (i >= y && dp[i-y] != -1) ? dp[i-y] + 1 : -1;
-            int c = (i >= z && dp[i-z] != -1) ? dp[i-z] + 1 : -1;
+        // Your code here
+        vector<int> dp(n+1,-1);
+        
+        dp[0] =0;
+        
+        for(int i=1;i<=n;i++){
+            int a = (i >= x && dp[i-x] != -1) ? dp[i-x]+1 : -1;
+            int b = (i >= y && dp[i-y] != -1) ? dp[i-y]+1 : -1;
+            int c = ( i >= z && dp[i-z] != -1 ) ? dp[i-z]+1 : -1;
             
-            dp[i] = max({a, b, c});
+            dp[i]= max({a,b,c});
         }
-
-        return max(dp[n], 0); // if no cut possible, return 0
+        return dp[n] == -1 ? 0 : dp[n];
     }
 };
