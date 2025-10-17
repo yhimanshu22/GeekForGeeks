e0d1a53bfc1604641d7ab6ef2308044056f6c1ab
Himanshu Yadav <himu09854@gmail.com>
2025-10-18 00:49:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -1,18 +1,25 @@
-class Solution {
-public:
-    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
-        int n = val.size();
-        vector<vector<int>> dp(n+1, vector<int>(capacity+1, 0));
+// User function Template for C++
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= capacity; j++) {
-                if (wt[i-1] <= j)
-                    dp[i][j] = max(dp[i-1][j], val[i-1] + dp[i][j - wt[i-1]]);
-                else
+class Solution {
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        int n = val.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(capacity+1,0));
+        
+        for(int i=0;i<=n;i++)dp[i][0] = 0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=capacity;j++){
+                if(wt[i-1] <= j){
+                    dp[i][j] = max( val[i-1] + dp[i][j-wt[i-1]] , dp[i-1][j]);
+                }else{
                     dp[i][j] = dp[i-1][j];
+                }
             }
         }
-
+        
         return dp[n][capacity];
     }
 };
