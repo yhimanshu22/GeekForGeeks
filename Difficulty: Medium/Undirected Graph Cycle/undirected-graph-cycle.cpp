2881d1809e8b67e64e89d56fcc8da6e4bfca4860
Himanshu Yadav <himu09854@gmail.com>
2025-10-21 01:52:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,47 +1,37 @@
 class Solution {
-public:
-    int findParent(int node, vector<int>& parent) {
-        if (parent[node] == node)
-            return node;
-        return parent[node] = findParent(parent[node], parent); // Path compression
-    }
-
-    void unionNodes(int u, int v, vector<int>& parent, vector<int>& rank) {
-        int pu = findParent(u, parent);
-        int pv = findParent(v, parent);
-
-        if (pu == pv) return;
-
-        // Union by rank
-        if (rank[pu] < rank[pv])
-            parent[pu] = pv;
-        else if (rank[pu] > rank[pv])
-            parent[pv] = pu;
-        else {
-            parent[pv] = pu;
-            rank[pu]++;
+  public:
+   int findParent(int node,vector<int>& parent){
+            if(parent[node] == node)return node;
+            return parent[node] = findParent(parent[node],parent);
         }
-    }
-
+        
+        bool unionSet(int u,int v,vector<int>& parent,vector<int>& rank){
+            int pu = findParent(u,parent);
+            int pv = findParent(v,parent);
+            
+            if(pu == pv)return true;//cycle detected
+            
+            if(rank[pu] < rank[pv]){
+                parent[pu] = pv;
+            }else if(rank[pv] < rank[pu]){
+                parent[pv] = pu;
+            }else{
+                parent[pv] = pu;
+                rank[pu]++;
+            }
+            return false;
+        }
     bool isCycle(int V, vector<vector<int>>& edges) {
-        vector<int> parent(V), rank(V, 0);
-
-        for (int i = 0; i < V; i++)
-            parent[i] = i;
-
-        for (auto &e : edges) {
-            int u = e[0];
-            int v = e[1];
-
-            int pu = findParent(u, parent);
-            int pv = findParent(v, parent);
-
-            if (pu == pv) // same set => cycle
-                return true;
-
-            unionNodes(u, v, parent, rank);
-        }
-
+       vector<int> parent(V) ,rank(V,0);
+       
+       for(int i=0;i<V;i++) parent[i]=i;
+       
+       for(auto& edge:edges){
+           int u = edge[0];
+           int v = edge[1];
+           
+           if(unionSet(u,v,parent,rank))return true;
+       }
         return false;
     }
 };
